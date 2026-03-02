3bf4a69fa536b0aa4ca932c73508e7380989f195
medexwilliam <william.wu@doh.nj.gov>
2026-03-02 16:03:04 -0500

    reorganized file for routes on top

--- rag/rag.py
+++ rag/rag.py
@@ -16,6 +16,12 @@ LANGUAGE_MODEL = 'hf.co/bartowski/Llama-3.2-1B-Instruct-GGUF'
 # Needs more than 4gb vram
 QWEN = 'hf.co/Qwen/Qwen2.5-7B-Instruct-GGUF:latest'
 
+# what do i need to be an api? there are somethings that don't necessarily need to be an api
+# anything that user interacts with the model (uploading things) :
+# 1. prompts/text
+# 1a. images (not yet)
+# i think its just the streaming of prompts back and forth. we don't need the underlying RAG logic to be exposed i think
+
 bm25: BM25 | None = None
 chunks: list[str] | None = None
 
@@ -32,19 +38,41 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-# what do i need to be an api? there are somethings that don't necessarily need to be an api
-# anything that user interacts with the model (uploading things) :
-# 1. prompts/text
-# 1a. images (not yet)
-# i think its just the streaming of prompts back and forth. we don't need the underlying RAG logic to be exposed i think
+@app.post('/chat', response_class=StreamingResponse)
+async def start_chat(prompt: UserPrompt):
+    
+    knowledge = await hybrid_retrieve(prompt.prompt)
+    print('Retrieved Knowledge:')
+    for chunk, similarity in knowledge:
+        print(f' - (rrf: {similarity:.4f}) {chunk}')
+        # print(f' - (similarity: {similarity:.4f}) {chunk}')
+    res = {}
+    res['knowledge'] = knowledge
+    res['res'] = []
+
+    instruction_prompt = f""" 
+    You are a fact retrieval assistant. You MUST answer ONLY using the facts listed below. 
+    If the answer is not in the list, say "I don't know."
+    Do NOT use any outside knowledge. Do NOT make things up.
+    {'\n'.join([f' - {chunk}' for chunk, _ in knowledge])}
+    """
+ 
+    stream = await client.chat(
+        # model=LANGUAGE_MODEL,
+        model=QWEN,
+        messages=[
+            {'role': 'system', 'content': instruction_prompt},
+            {'role': 'user', 'content': prompt.prompt},
+        ],
+        stream=True
+    ) 
+
+    async for chunk in stream:
+        yield chunk['message']['content']
+    # res['status'] = 'success'
+    # return res
 
 client = ollama.AsyncClient(host="http://host.docker.internal:11434")
-# line by line chunking strategy
-async def load_data(filepath: str) -> list[str]:
-    with open(filepath) as f:
-        dataset = f.readlines()
-        print(f"Loading {len(dataset)} entries...")
-        return dataset
 
 async def chunk_data(filepath: str) -> list[str]:
     """
@@ -56,26 +84,6 @@ async def chunk_data(filepath: str) -> list[str]:
         chunked_data = splitter.split_text(f.read())
         print(f"Loading {len(chunked_data)} entries...")
         return chunked_data
-
-async def add_dataset_to_db(data_set: list[str]) -> None:
-    # Regular embedding without rrf
-    async with get_conn() as conn:
-        async with conn.cursor() as cur:
-            await cur.execute("TRUNCATE TABLE chunks")
-            # chunking strategy of one line
-            for i, chunk in enumerate(data_set): 
-                embeddings = await client.embed(EMBEDDING_MODEL, input=chunk)
-                embedding = embeddings['embeddings'][0]
-                
-                await cur.execute(
-                    """
-                    INSERT INTO chunks
-                    (content, embedding, metadata) 
-                    VALUES (%s, %s, %s) 
-                    """,
-                    (chunk, embedding, Jsonb({}))
-                ) 
-                print(f"Added chunk {i+1}/{len(data_set)} to vector db")
 
 async def add_dataset_to_db_hybrid(data_set: list[str]) -> None:
     async with get_conn() as conn:
@@ -144,6 +152,48 @@ async def hybrid_retrieve(query: str, top:int=3) -> list[tuple]:
     doc_scores = sorted(doc_scores, key=lambda x: x[0], reverse=True)[:top]
     return await rrf(semantic_res, doc_scores)
 
+# Limitations from what i learned
+"""
+The model actually ignores or doesn't follow instruction commands very well. it either ignores it completely or ocassionally gives a context answer
+This is dependent on the user query.
+
+This limitation comes from the model parameter size of 1B. Tiny models are really notorious for not following instruction prompts. 
+"""
+"""
+cosine similarity measures the similarity between two non zero vectors by calculating the cosine angle between them.
+The range is -1 to 1. 1 is identical. -1 is exactly opposite 
+helps find document similarity regardless of length
+semantic search for vector embeddings closest to user's query
+"""
+
+# SYNCHRONOUS/OLD FOR COMPARISON
+
+# line by line chunking strategy
+async def load_data(filepath: str) -> list[str]:
+    with open(filepath) as f:
+        dataset = f.readlines()
+        print(f"Loading {len(dataset)} entries...")
+        return dataset
+
+async def add_dataset_to_db(data_set: list[str]) -> None:
+    # Regular embedding without rrf
+    async with get_conn() as conn:
+        async with conn.cursor() as cur:
+            await cur.execute("TRUNCATE TABLE chunks")
+            # chunking strategy of one line
+            for i, chunk in enumerate(data_set): 
+                embeddings = await client.embed(EMBEDDING_MODEL, input=chunk)
+                embedding = embeddings['embeddings'][0]
+                
+                await cur.execute(
+                    """
+                    INSERT INTO chunks
+                    (content, embedding, metadata) 
+                    VALUES (%s, %s, %s) 
+                    """,
+                    (chunk, embedding, Jsonb({}))
+                ) 
+                print(f"Added chunk {i+1}/{len(data_set)} to vector db")
 
 # obtains top x similarities in search and returns them
 async def retrieve(query: str, top:int=3) -> list[tuple]:
@@ -163,51 +213,3 @@ async def retrieve(query: str, top:int=3) -> list[tuple]:
             )
             # no need to convert?
             return await cur.fetchall()
-
-@app.post('/chat', response_class=StreamingResponse)
-async def start_chat(prompt: UserPrompt):
-    
-    knowledge = await hybrid_retrieve(prompt.prompt)
-    print('Retrieved Knowledge:')
-    for chunk, similarity in knowledge:
-        print(f' - (rrf: {similarity:.4f}) {chunk}')
-        # print(f' - (similarity: {similarity:.4f}) {chunk}')
-    res = {}
-    res['knowledge'] = knowledge
-    res['res'] = []
-
-    instruction_prompt = f""" 
-    You are a fact retrieval assistant. You MUST answer ONLY using the facts listed below. 
-    If the answer is not in the list, say "I don't know."
-    Do NOT use any outside knowledge. Do NOT make things up.
-    {'\n'.join([f' - {chunk}' for chunk, _ in knowledge])}
-    """
- 
-    stream = await client.chat(
-        # model=LANGUAGE_MODEL,
-        model=QWEN,
-        messages=[
-            {'role': 'system', 'content': instruction_prompt},
-            {'role': 'user', 'content': prompt.prompt},
-        ],
-        stream=True
-    ) 
-
-    async for chunk in stream:
-        yield chunk['message']['content']
-    # res['status'] = 'success'
-    # return res
-
-# Limitations from what i learned
-"""
-The model actually ignores or doesn't follow instruction commands very well. it either ignores it completely or ocassionally gives a context answer
-This is dependent on the user query.
-
-This limitation comes from the model parameter size of 1B. Tiny models are really notorious for not following instruction prompts. 
-"""
-"""
-cosine similarity measures the similarity between two non zero vectors by calculating the cosine angle between them.
-The range is -1 to 1. 1 is identical. -1 is exactly opposite 
-helps find document similarity regardless of length
-semantic search for vector embeddings closest to user's query
-"""
